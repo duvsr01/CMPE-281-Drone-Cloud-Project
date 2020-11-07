9de85e51df6fd7aff3a16c46aaacae0380f93f15
sinnanje <sahana.innanje@sjsu.edu>
2020-11-07 08:15:27 -0800

    changes - boiler plate code - initial setup to connect to backend - needs refactoring

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express();
 const port = 3001;
 
+var cors = require('cors')
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('Test connection to backend!!')
 });
+
+
+app.post('/signup',function(req,res){
+  console.log(req.body);
+  console.log("sign up");
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
